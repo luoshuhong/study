516d38c27c6cc5911665a1a758a87da320d265e3
luoshuhong <luoshuhong@meituan.com>
2019-05-16 09:39:20 +0800

    no message

---
+++ test-all/src/main/java/com/lsh/leetcode/MainTest.java
@@ -0,0 +1,10 @@
+package com.lsh.leetcode;
+
+/**
+ * Created by luoshuhong on 2019/5/16.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("leetcode 20190516");
+    }
+}
